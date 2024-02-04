2c841c31cee9c1e453fcaa9ca4156faf484deb99
gerson aguiar <gersoncafilho@gmail.com>
2024-02-04 17:45:05 -0300

    install MUI and use basic component button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import Button from '@mui/material/Button';
+
+function ButtonUsage() {
+  return <Button variant="contained">Hello world</Button>;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ButtonUsage />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
