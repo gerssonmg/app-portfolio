0b7690ee6033f9af089f2f1f60989acf2345f9ec
gerson aguiar <gersoncafilho@gmail.com>
2024-02-04 20:49:20 -0300

    short command 20240204

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
-import Button from '@mui/material/Button';
 // import AppRoot from './app/index';
 import Blog from './blog/Blog';
 
